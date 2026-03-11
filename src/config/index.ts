1524faed2c9cc7eecfc888500bfc2c3408d78260
Luis Carlos Pacheco Aldana <141878022+LuisCaPacheco@users.noreply.github.com>
2026-03-11 09:58:17 -0500

    Delete src directory

--- src/config/index.ts
+++
@@ -1,6 +0,0 @@
-import appConfig from './app.config';
-import databaseConfig from './database.config';
-import jwtConfig from './jwt.config';
-import swaggerConfig from './swagger.config';
-
-export default [appConfig, databaseConfig, jwtConfig, swaggerConfig];
--- src/config/jwt.config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('jwt', () => ({
-  secret: process.env.JWT_SECRET,
-  refreshSecret: process.env.JWT_REFRESH_SECRET,
-  expiresIn: process.env.JWT_EXPIRATION || '15m',
-  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRATION || '7d',
-}));
